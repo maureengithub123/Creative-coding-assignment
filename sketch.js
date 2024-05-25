b3abc41a003f52331978ec2bb24674c7b89185a7
maureengithub123 <168146899+maureengithub123@users.noreply.github.com>
2024-05-25 20:13:50 +1000

    Made water patterns and turtle

--- sketch.js
+++ sketch.js
@@ -1,10 +1,12 @@
-let colors = ['black', 'White', 'blue', '#pink', 'yellow', 'green', '#f477c3', '#70499c', '#006494', '#1b98e0'];
-let dotSize = 4; // 控制点的大小，您可以自由调整这个值
+// goal, simple color dreamtime feeling, pop and rotate
+
+let colors = ['#6e85e0', 'White', '#7b6ee0', '#35b84d', '#8dd1e0','#dee08d','#eaccf0', '#9839a8'];
+let dotSize = 6.5; // 控制点的大小，您可以自由调整这个值
 let asset;
 
 
 function preload(){
-asset = loadSound();
+asset = loadSound("assets/digeridoo1.mp3");
 
 
 }
@@ -15,36 +17,63 @@ function setup() {
   rectMode(CENTER);
 
 
+
   randomSeed(999); // 固定随机种子值
 
   translate(width / 2, height / 2);
   rotate(PI * random(10)); 
   translate(-width / 2, -height / 2);
-  background('#014b70');
+  background('black');
+  
 
+  switch (width) {
+    case "pop":
+       break;
+    case "rotate":
+       break;
+  }
+
+
+ 
 
   // 手动定义的位置来填充画布
   let positions = [
-    {x:  width / 2, y: height / 2, size: 20},
-    {x: width / 2, y: height / 2, size: 20},
-    {x: 3 * width / 4, y: 3 * height / 4, size: 20},
-    {x: width / 4, y: 3 * height / 4, size: 20},
-    {x: 3 * width / 4, y: height / 4, size: 20},
-    {x: width / 4, y: height / 2, size: 20},
-    {x: 3 * width / 4, y: height / 2, size: 20},
-    {x: width / 2, y: height / 4, size: 20},
-    {x: width / 2, y: 3 * height / 4, size: 20},
-    {x: width / 6, y: height / 6, size: 20},
-    {x: 5 * width / 6, y: 5 * height / 6, size: 20},
-    {x: width / 6, y: 5 * height / 6, size: 20},
-    {x: 5 * width / 6, y: height / 6, size: 20},
-    {x: width / 6, y: height / 2, size: 20},
-    {x: 5 * width / 6, y: height / 2, size: 20},
-    {x: width / 2, y: height / 6, size: 20},
-    {x: width / 2, y: 5 * height / 6, size: 20},
-    {x: width / 3, y: height / 3, size: 20}
+    {x:20, y: 200, size: 15},
+    {x:160, y: 180, size: 15},
+    {x:320, y: 190, size: 15},
+    {x:460, y: 190, size: 15},
+    {x:380, y: 50, size: 15},
+    {x:450, y: 350, size: 15},
+    {x:590, y: 270, size: 15},
+    {x:50, y: 30, size: 15},
+    {x:220, y: 40, size: 15},
+    {x:-130, y: 280, size: 15},
+    {x:20, y: 300, size: 15},
+    {x:200, y: 590, size: 15},
+
+
+    // Turtle
+ 
+    {x:220, y: 400, size: 5},
+    {x:150, y: 390, size: 5},
+
+
+
 
   ];
+
+  stroke('#9aecfc')
+  strokeWeight(3)
+  fill('#182557')
+  ellipse(330,400,90,80), // head
+  ellipse(290,330,90,40), // arm 1
+  ellipse(100,480,90,40), // foot 2
+  ellipse(290,480,90,40), // arm 2
+  ellipse(120,300,90,40), // foot 1
+  fill('black')
+ellipse(190,400,250,190), // body
+{x:20, y: 300, size: 15},
+
 
   positions.forEach(pos => {
     drawPattern(pos.x, pos.y, pos.size);
@@ -66,6 +95,7 @@ function form(x, y, d, num, mn) {
   }
   endShape(CLOSE);
   pop();
+ 
 }
 
 function drawPattern(x, y, s) {
@@ -78,7 +108,7 @@ function drawPattern(x, y, s) {
     form(x, y, dd, rgn, mn);
   }
   for (let i = 0; i < 5; i++) {
-    drawDots(x, y, s + 0.1 + i * 1, random(colors));
+    drawDots(x, y, s + 0.1 + i * 35, random(colors));
   }
   drawConcentricCircles(x, y, 3, 20); 
 }
@@ -86,13 +116,15 @@ function drawPattern(x, y, s) {
 function drawDots(x, y, diameter, color) {
   let numDots = 40; // 点的数量增加以使环更密集
   fill(color);
-  noStroke();
+  stroke('black');
+  strokeWeight(3);
   for (let i = 0; i < numDots; i++) {
     let angle = TWO_PI / numDots * i;
     let dotX = x + cos(angle) * diameter / 2;
     let dotY = y + sin(angle) * diameter / 2;
     ellipse(dotX, dotY, dotSize, dotSize); // 使用全局变量 dotSize 控制点的大小
  
+  
 }
 }
 
@@ -108,3 +140,4 @@ function drawConcentricCircles(x, y, numCircles, initialDiameter) {
   }
 }
 
+//
